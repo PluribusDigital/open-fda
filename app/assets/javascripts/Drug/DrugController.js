8ad4d5f1de7cc3752e43e2545ed2e5a1e7d57a45
Ben Morris <github@benmorris.net>
2015-06-17 16:07:15 -0400

    basic wiring to update drug detail based on search result

--- app/assets/javascripts/Drug/DrugController.js
+++ app/assets/javascripts/Drug/DrugController.js
@@ -1,16 +1,26 @@
 app.controller("DrugController", ['$scope', '$http', function ($scope, $http) {
 
+  window.DrugControllerScope = $scope;
+  $scope.selectedDrug, $scope.drug = null;
 
-  $scope.getLocation = function(val) {
+
+  // typeahead search
+  $scope.searchDrugs = function(val) {
     return $http.get('/drugs.json', {
       params: {
         name: val
       }
     }).then(function(response){
       return response.data.results.map(function(item){
+        $scope.selectedDrug = item;
         return item.name;
       });
     });
   };
+
+  // fetch all relevant details for a given drug
+  $scope.getDetail = function() {
+    $scope.drug = $scope.selectedDrug;
+  }
 
 }]);
