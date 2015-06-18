d8f4e6f0763ca24049875a6d8e2570d7418f483c
Ben Morris <github@benmorris.net>
2015-06-18 17:32:36 -0400

    complete service_cache specs

--- spec/models/service_cache_spec.rb
+++ spec/models/service_cache_spec.rb
@@ -7,9 +7,25 @@ RSpec.describe ServiceCache do
 
   describe "saving" do
 
-    it "should save a record"
+    it "should save a record" do 
+      expect{
+        TestService.write_cache "apple", {name: 'apple', color:'red', size:'medium'}
+      }.to change{ServiceCache.count}.by 1
+    end
 
-    it "should update a duplicate record"
+    it "should assign the class name as the service name" do 
+      expect(ServiceCache.where(:service => "TestService").count).to eq 0
+      TestService.write_cache "apple", {name: 'apple', color:'red', size:'medium'}
+      expect(ServiceCache.where(:service => "TestService").count).to eq 1
+    end
+
+    it "should update a duplicate record, simply updating attributes" do 
+      expect{
+        TestService.write_cache "apple", {name: 'apple', color:'red', size:'medium'}
+        TestService.write_cache "apple", {name: 'apple', color:'reddish', size:'medium'}
+      }.to change{ServiceCache.count}.by 1
+      expect( TestService.find("apple")['color'] ).to eq 'reddish'
+    end
 
   end
 
