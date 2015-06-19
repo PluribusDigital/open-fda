2de3ef1c294a28748b22950fb8a427ed822e2eea
Chris Cassatt <chris.cassatt@gmail.com>
2015-06-19 10:35:35 -0400

    Debugging deployment failure

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,19 @@
 # deploy.sh
 #! /bin/bash
 
-SHA1=$1
+#SHA1=$1
 
 # Deploy image to Docker Hub
 #docker push circleci/hello:$SHA1
 
 # Create new Elastic Beanstalk version
-EB_BUCKET=stsi-sandbox
+#EB_BUCKET=stsi-sandbox
 #DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 #sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 #aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name open-fda-dempo \
-  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=eb-docker-nginx-proxy.zip
+#aws elasticbeanstalk create-application-version --application-name open-fda-dempo \
+#  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=eb-docker-nginx-proxy.zip
 
 # Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name phpTestApp-stsi-env \
-    --version-label $SHA1
+#aws elasticbeanstalk update-environment --environment-name phpTestApp-stsi-env \
+#    --version-label $SHA1
