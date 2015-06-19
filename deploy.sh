156104466b1f51e951860bb9c90b64c3e03d792f
Chris Cassatt <chris.cassatt@gmail.com>
2015-06-19 11:29:40 -0400

    fixed deploy.sh permission error
    Windows is most likely omitting the execute permission on deploy.sh so
    we'll execute using bash command for now.

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,9 @@ EB_BUCKET=stsi-sandbox
 #sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 #aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
 aws elasticbeanstalk create-application-version --application-name open-fda-dempo \
-  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=eb-docker-nginx-proxy.zip
+  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=eb-docker-nginx-proxy.zip \
+  --region us-east-1
 
 # Update Elastic Beanstalk environment to new version
 aws elasticbeanstalk update-environment --environment-name phpTestApp-stsi-env \
-    --version-label $SHA1
+    --version-label $SHA1 --region us-east-1
