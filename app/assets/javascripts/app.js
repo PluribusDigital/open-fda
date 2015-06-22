aaf3227912ac99f8d44291d96535d614334257f3
Jeff <jtholden@gmail.com>
2015-06-22 15:01:50 -0400

    Add utility functions for formatting dates and the recall class.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -22,4 +22,35 @@ app.config([
 }
 ]);
 
+app.run(function($rootScope) {
+
+  //app util functions
+  $rootScope.UTIL = {
+
+    //Parse date in YYYYmmdd format and return date
+    parseDate: function(dateString) {
+      if(!/^(\d){8}$/.test(dateString)) return "invalid date";
+      var y = dateString.substr(0,4),
+          m = dateString.substr(4,2) - 1,
+          d = dateString.substr(6,2);
+      return new Date(y, m, d);
+    },
+
+    //template strings to change class to colorize.
+    classifyRecall: function(recallClassification) {
+      switch (recallClassification) {
+        case "Class I":
+          return "danger";
+        case "Class II":
+          return "warning";
+        case "Class III":
+          return "info";
+        default:
+          return "";
+      }
+    }
+
+  };
+});
+
 var directives  = angular.module('directives', []);
