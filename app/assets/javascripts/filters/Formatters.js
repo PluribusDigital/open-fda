18fcd59176b1c53ef2fa7151dccb881377ff5502
Jeff <jtholden@gmail.com>
2015-06-22 16:53:04 -0400

    Fix link to fda website.

--- app/assets/javascripts/filters/Formatters.js
+++ app/assets/javascripts/filters/Formatters.js
@@ -7,3 +7,13 @@ app.filter('humanizeFdaDate',function() {
     return m + "/" + d + "/" + y + ""; 
   };
 });
+
+app.filter('dmyDate', function() {
+  return function(dateString) {
+    var y,m,d;
+    y = dateString.substring(0,4);
+    m = dateString.substring(4,6);
+    d = dateString.substring(6,8);
+    return m + d + y;
+  };
+});
