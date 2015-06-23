8df07a1755cb86685b2d198df740c6afebcc520f
Chris Cassatt <chris.cassatt@gmail.com>
2015-06-23 10:25:00 -0400

    selectively deploy docker images

--- docker_build.sh
+++ docker_build.sh
@@ -1,20 +1,16 @@
 #! /bin/bash   
 set -e
 
-SHA1=$CIRCLE_SHA1
-
 docker info
 
 # build web image
-docker build -t stsilabs/openfda-web:$SHA1 .
+docker build -t stsilabs/openfda-web:$CIRCLE_SHA1 .
     
 # run docker containers
-docker run --name openfda-postgres -e POSTGRES_USER=$POSTGRES_USER -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -d postgres:9.4
-docker run --link openfda-postgres:postgres -p 80:80 -e "RAILS_ENV=demo" --name openfda-web stsilabs/openfda-web:$SHA1 bundle exec rake db:setup
- 
-# create image
-docker commit openfda-postgres stsilabs/openfda-postgres
-
-# push to hub
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWORD
-docker push stsilabs/openfda-postgres
+if [ "$BUILD_POSTGRES_IMAGE" = "true"  ]
+then
+  docker run --name openfda-postgres -e POSTGRES_USER=$POSTGRES_USER -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -d postgres:9.4
+  docker run --link openfda-postgres:postgres -p 80:80 -e "RAILS_ENV=demo" --name openfda-web stsilabs/openfda-web:$CIRCLE_SHA1 bundle exec rake db:setup
+  # create image
+  docker commit openfda-postgres stsilabs/openfda-postgres
+fi
--- docker_deploy.sh
+++ docker_deploy.sh
@@ -1,11 +1,16 @@
 #! /bin/bash
 set -e
 
-SHA1=$CIRCLE_SHA1
 DOCKERRUN_FILE=Dockerrun.aws.json
 
 # Deploy web image to Docker Hub
-docker push stsilabs/openfda-web:$SHA1
+docker push stsilabs/openfda-web:$CIRCLE_SHA1
+
+# Deploy postgres image to Docker Hub
+if [ "$BUILD_POSTGRES_IMAGE" = "true"  ]
+then
+  docker push stsilabs/openfda-postgres
+fi
 
 # Optionally deploy postgres image
 #if [ "$BUILD_POSTGRES_IMAGE" = "true"  ]
@@ -17,16 +22,16 @@ docker push stsilabs/openfda-web:$SHA1
 EB_BUCKET=open-fda
 
 # variable substitutions
-sed -e "s/<TAG>/$SHA1/" -e "s/<POSTGRES_USER>/$POSTGRES_USER/" -e "s/<POSTGRES_PASSWORD>/$POSTGRES_PASSWORD/" < $DOCKERRUN_FILE.template > $DOCKERRUN_FILE
+sed -e "s/<TAG>/$CIRCLE_SHA1/" -e "s/<POSTGRES_USER>/$POSTGRES_USER/" -e "s/<POSTGRES_PASSWORD>/$POSTGRES_PASSWORD/" < $DOCKERRUN_FILE.template > $DOCKERRUN_FILE
 
 # elastic beanstalk requires application source to be zipped
 zip $DOCKERRUN_FILE.zip $DOCKERRUN_FILE
 
 aws s3 cp $DOCKERRUN_FILE.zip s3://$EB_BUCKET/$DOCKERRUN_FILE.zip
 aws elasticbeanstalk create-application-version --application-name open-fda-stsi \
-  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE.zip \
+  --version-label $CIRCLE_SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE.zip \
   --region us-east-1
 
 # Update Elastic Beanstalk environment to new version
 aws elasticbeanstalk update-environment --environment-name open-fda-stsi \
-    --version-label $SHA1 --region us-east-1
+    --version-label $CIRCLE_SHA1 --region us-east-1
