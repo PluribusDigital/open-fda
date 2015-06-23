3b8fb50f89db305c80ffa6250e660b453740eb93
Jeffrey M Farley <JeffreyMFarley@users.noreply.github.com>
2015-06-23 17:12:00 -0400

    Step 1: The Adverse Reactions table as a directive

--- app/assets/javascripts/directives/D3Charts.js
+++ app/assets/javascripts/directives/D3Charts.js
@@ -1,136 +1,36 @@
-// TODO: clean up / create new directives
-var directives = angular.module('directives');
+app.controller('D3BarController', function ($scope) {
+    $scope.event_data = [];
 
-directives.directive("barChart", ['d3Service', function (d3Service) {
-    return {
-      restrict: 'EA',
-      scope: {},
-      link: function(scope, element, attrs) {
-        d3Service.d3().then(function(d3) {
-          var svg = d3.select(ele[0])
-            .append('svg')
-            .style('width', '100%');
+    $scope.onLoaded = function (data) {
+        if (data == null)
+            return;
 
-          // Browser onresize event
-          window.onresize = function() {
-            scope.$apply();
-          };
+        $scope.event_data = data;
+    };
 
-          // hard-code data
-          scope.data = [
-            {name: "Greg", score: 98},
-            {name: "Ari", score: 96},
-            {name: 'Q', score: 75},
-            {name: "Loser", score: 48}
-          ];
-
-          // Watch for resize event
-          scope.$watch(function() {
-            return angular.element($window)[0].innerWidth;
-          }, function() {
-            scope.render(scope.data);
-          });
-
-          scope.render = function(data) {
-            // our custom d3 code
-          }
-        });
-      }};
-  }]);
+    $scope.onClick = function (term) {
+        $scope.clickTarget()(term);
+    }
+});
 
 
-app.directive('linearChart', function($parse, $window){
-   return{
-      restrict:'EA',
-      template:"<svg width='850' height='200'></svg>",
-       link: function(scope, elem, attrs){
-           var exp = $parse(attrs.chartData);
+app.directive('barchart', function () {
+    var chart = {
+        restrict: 'EA',
+        scope: {
+            series: "=",
+            clickTarget: "&"
+        },
+        template: '<table class="table"><tbody><tr ng-repeat="datum in event_data"><th>{{datum.term}}</th><td><a href="" ng-click="onClick(datum.term)">{{datum.count}}</a></td></tr></tbody></table>',
+        controller: 'D3BarController',
+        link: function (scope, element, attrs) {
+            // watch for the value to bind
+            scope.$watch('series', function (newValue, oldValue) {
+                if (newValue)
+                    scope.onLoaded(newValue);
+            });
+        }
+    }
 
-           var salesDataToPlot=exp(scope);
-           var padding = 20;
-           var pathClass="path";
-           var xScale, yScale, xAxisGen, yAxisGen, lineFun;
-
-           var d3 = $window.d3;
-           var rawSvg=elem.find('svg');
-           var svg = d3.select(rawSvg[0]);
-
-           scope.$watchCollection(exp, function(newVal, oldVal){
-               salesDataToPlot=newVal;
-               redrawLineChart();
-           });
-
-           function setChartParameters(){
-
-               xScale = d3.scale.linear()
-                   .domain([salesDataToPlot[0].hour, salesDataToPlot[salesDataToPlot.length-1].hour])
-                   .range([padding + 5, rawSvg.attr("width") - padding]);
-
-               yScale = d3.scale.linear()
-                   .domain([0, d3.max(salesDataToPlot, function (d) {
-                       return d.sales;
-                   })])
-                   .range([rawSvg.attr("height") - padding, 0]);
-
-               xAxisGen = d3.svg.axis()
-                   .scale(xScale)
-                   .orient("bottom")
-                   .ticks(salesDataToPlot.length - 1);
-
-               yAxisGen = d3.svg.axis()
-                   .scale(yScale)
-                   .orient("left")
-                   .ticks(5);
-
-               lineFun = d3.svg.line()
-                   .x(function (d) {
-                       return xScale(d.hour);
-                   })
-                   .y(function (d) {
-                       return yScale(d.sales);
-                   })
-                   .interpolate("basis");
-           }
-         
-         function drawLineChart() {
-
-               setChartParameters();
-
-               svg.append("svg:g")
-                   .attr("class", "x axis")
-                   .attr("transform", "translate(0,180)")
-                   .call(xAxisGen);
-
-               svg.append("svg:g")
-                   .attr("class", "y axis")
-                   .attr("transform", "translate(20,0)")
-                   .call(yAxisGen);
-
-               svg.append("svg:path")
-                   .attr({
-                       d: lineFun(salesDataToPlot),
-                       "stroke": "blue",
-                       "stroke-width": 2,
-                       "fill": "none",
-                       "class": pathClass
-                   });
-           }
-
-           function redrawLineChart() {
-
-               setChartParameters();
-
-               svg.selectAll("g.y.axis").call(yAxisGen);
-
-               svg.selectAll("g.x.axis").call(xAxisGen);
-
-               svg.selectAll("."+pathClass)
-                   .attr({
-                       d: lineFun(salesDataToPlot)
-                   });
-           }
-
-           drawLineChart();
-       }
-   };
+    return chart;
 });
