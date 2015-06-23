eddcc1a4ea61301ea809ceb66e9a0ff8412ed8e1
Chris Cassatt <chris.cassatt@gmail.com>
2015-06-23 00:53:32 -0400

    fixed

--- docker_build.sh
+++ docker_build.sh
@@ -4,15 +4,18 @@ POSTGRES_USER=$2
 POSTGRES_PASSWORD=$3
 BUILD_POSTGRES_IMAGE=$4
 
-docker info
+if [ "$BUILD_POSTGRES_IMAGE" = "true"  ] 
+then 
+  docker info
 
-# build web image
-docker build -t stsilabs/openfda-web:$CIRCLE_SHA1 .
+  # build web image
+  docker build -t stsilabs/openfda-web:$SHA1 .
     
-# run docker containers
-docker run --name openfda-postgres -e POSTGRES_USER=$POSTGRES_USER -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -d postgres:9.4
-docker run --link openfda-postgres:postgres -p 80:80 -e "RAILS_ENV=demo" --name openfda-web stsilabs/openfda-web:$SHA1 bundle exec rake db:setup
-docker run --link openfda-postgres:postgres -p 80:80 -e "RAILS_ENV=demo" --name openfda-web stsilabs/openfda-web:$CIRCLE_SHA1 bundle exec rake db:setup:
+  # run docker containers
+  docker run --name openfda-postgres -e POSTGRES_USER=$POSTGRES_USER -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -d postgres:9.4
+  docker run --link openfda-postgres:postgres -p 80:80 -e "RAILS_ENV=demo" --name openfda-web stsilabs/openfda-web:$SHA1 bundle exec rake db:setup
+  docker run --link openfda-postgres:postgres -p 80:80 -e "RAILS_ENV=demo" --name openfda-web stsilabs/openfda-web:$SHA1 bundle exec rake db:setup:
 
-# create image
-docker commit postgres stsilabs/openfda-postgres
+  # create image
+  docker commit postgres stsilabs/openfda-postgres
+fi
