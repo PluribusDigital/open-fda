14618bccd6d5b9d714c61c619210eb056349d353
Chris Cassatt <chris.cassatt@gmail.com>
2015-06-22 22:54:02 -0400

    Fixed missing version from image name

--- docker_build.sh
+++ docker_build.sh
@@ -11,7 +11,7 @@ docker build -t stsilabs/openfda-web:$CIRCLE_SHA1 .
     
 # run docker containers
 docker run --name openfda-postgres -e POSTGRES_USER=$POSGRES_USER -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -d postgres:9.4
-docker run --link openfda-postgres:postgres -p 80:80 -e "RAILS_ENV=demo" --name openfda-web stsilabs/openfda-web bundle exec rake db:setup
+docker run --link openfda-postgres:postgres -p 80:80 -e "RAILS_ENV=demo" --name openfda-web stsilabs/openfda-web:$SHA1 bundle exec rake db:setup
  
 #build postgres image   
 if [ "$BUILD_POSTGRES_IMAGE" = "true"  ]; then docker commit postgres stsilabs/openfda-postgres; fi
