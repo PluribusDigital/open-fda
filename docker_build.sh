6d5b2b551ecbef680917d8f215b5b789126ee140
Chris Cassatt <chris.cassatt@gmail.com>
2015-06-24 01:23:52 -0400

    docker login

--- docker_build.sh
+++ docker_build.sh
@@ -13,6 +13,6 @@ then
   docker run --link openfda-postgres:postgres -p 80:80 -e "RAILS_ENV=demo" --name openfda-web stsilabs/openfda-web:$CIRCLE_SHA1 bundle exec rake db:setup
   # create image
   docker commit openfda-postgres stsilabs/openfda-postgres:$OPENFDA_POSTGRES_VERSION
-  docker ps
+  docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWORD
   docker push stsilabs/openfda-postgres:$OPENFDA_POSTGRES_VERSION
 fi
