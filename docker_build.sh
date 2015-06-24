cd9255e56c8ad18c1a768481d2e64b90fe8206de
Chris Cassatt <chris.cassatt@gmail.com>
2015-06-24 00:54:10 -0400

    debugging docker push failure

--- docker_build.sh
+++ docker_build.sh
@@ -12,5 +12,7 @@ then
   docker run --name openfda-postgres -e POSTGRES_USER=$POSTGRES_USER -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -d postgres:9.4
   docker run --link openfda-postgres:postgres -p 80:80 -e "RAILS_ENV=demo" --name openfda-web stsilabs/openfda-web:$CIRCLE_SHA1 bundle exec rake db:setup
   # create image
-  docker commit openfda-postgres stsilabs/openfda-postgres:$DOCKER_DATA_VERSION
+  docker commit openfda-postgres stsilabs/openfda-postgres:$OPENFDA_POSTGRES_VERSION
+  docker ps
+  docker push stsilabs/openfda-postgres:$OPENFDA_POSTGRES_VERSION
 fi
