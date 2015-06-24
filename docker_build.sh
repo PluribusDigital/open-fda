c837e44956bb51fcf88022ac4116dc2566c651c3
Chris Cassatt <chris.cassatt@gmail.com>
2015-06-24 05:27:36 -0400

    fix

--- docker_build.sh
+++ docker_build.sh
@@ -9,8 +9,11 @@ docker build -t stsilabs/openfda-web:$CIRCLE_SHA1 .
 # run docker containers
 if [ "$BUILD_POSTGRES_IMAGE" = "true"  ]
 then
-  docker run --name openfda-postgres -e POSTGRES_USER=$POSTGRES_USER -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -d postgres:9.4
+  docker run --name openfda-postgres -e POSTGRES_USER=$POSTGRES_USER -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -d stsilabs/postgres
   docker run --link openfda-postgres:postgres -p 80:80 -e "RAILS_ENV=demo" --name openfda-web stsilabs/openfda-web:$CIRCLE_SHA1 bundle exec rake db:setup
   # create image
   docker commit openfda-postgres stsilabs/openfda-postgres:$OPENFDA_POSTGRES_VERSION
+  docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWORD
+  docker ps
+  docker push stsilabs/openfda-postgres:$OPENFDA_POSTGRES_VERSION
 fi
