dbba3a5e08c641b3ca6281b64fa414f4a57856fb
Jeffrey M Farley <JeffreyMFarley@users.noreply.github.com>
2015-06-25 15:24:45 -0400

    Unit test for Drugs angular service

---
+++ spec/javascripts/services/DrugService_spec.js
@@ -0,0 +1,46 @@
+﻿describe("DrugService", function () {
+    var target, httpBackend;
+
+    beforeEach(module("openFDA"));
+    beforeEach(inject(function (_DrugService_, $httpBackend) {
+        target = _DrugService_;
+        httpBackend = $httpBackend;
+        httpBackend.resetExpectations();
+    }));
+
+    afterEach(function () {
+        httpBackend.verifyNoOutstandingExpectation();
+        httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('is created on module initialization', function () {
+        expect(target).not.toBeNull();
+    });
+
+    it('supports the type ahead search', function () {
+        url = '/api/v1/drugs.json?q=v';
+        httpBackend.expectGET(url).respond({ 'results': [{'name': 'x'}]});
+
+        target.typeAheadSearch('v')
+            .then(function (result) {
+                expect(result).not.toBeNull();
+                expect(result.length).toEqual(1);
+                expect(result[0].name).toBeDefined();
+            });
+
+        httpBackend.flush();
+    });
+
+    it('responds gracefully when the server is not available during type ahead search', function () {
+        url = '/api/v1/drugs.json?q=v';
+        httpBackend.expectGET(url).respond(404, '');
+
+        target.typeAheadSearch('v')
+            .then(function (result) {
+                expect(result).not.toBeNull();
+                expect(result.length).toEqual(0);
+            });
+
+        httpBackend.flush();
+    });
+});
