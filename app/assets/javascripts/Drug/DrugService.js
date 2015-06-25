c1612ef8a3ccfc749b8feab4ce66e5528ff0bf4c
Jeffrey M Farley <JeffreyMFarley@users.noreply.github.com>
2015-06-25 12:19:30 -0400

    Moved $http calls to services

---
+++ app/assets/javascripts/Drug/DrugService.js
@@ -0,0 +1,35 @@
+﻿app.factory('DrugService',
+    function ($http) {
+        'use strict';
+
+        var service = {
+           error: function (response) {
+                console.log('error when calling Drugs API');
+                console.log(response);
+                return [];
+            },
+
+            searchSuccess: function (response) {
+                return response.data.results.map(function (item) {
+                    return item;
+                });
+            },
+
+            // These methods encapsulate API calls
+            typeAheadSearch: function (val) {
+                return $http.get('/api/v1/drugs.json', {
+                    params: {
+                        q: val
+                    }
+                }).then(this.searchSuccess, this.error);
+            },
+
+            getDetails: function (product_ndc, success) {
+                $http.get('/api/v1/drugs/' + product_ndc, {})
+                     .then(function (response) { success(response.data); }, this.error);
+            }
+
+        };
+
+        return service;
+    });
---
+++ app/assets/javascripts/Drug/EventService.js
@@ -0,0 +1,21 @@
+﻿app.factory('EventService',
+    function ($http) {
+        'use strict';
+
+        var service = {
+            error: function (response) {
+                console.log('error when calling Events API');
+                console.log(response);
+                return [];
+            },
+
+            // These methods encapsulate API calls
+            index: function (brand, symptom, success) {
+                $http.get('/api/v1/events?brand_name=' + brand + '&term=' + symptom, {}
+                ).then(function (response) { success(response.data); }, this.error);
+            }
+
+        };
+
+        return service;
+    });
