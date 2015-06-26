9a17e31c21f879a6cf81be7308407040de5bc529
Jeffrey M Farley <JeffreyMFarley@users.noreply.github.com>
2015-06-26 15:32:37 -0400

    Search box moved to nav

--- app/assets/javascripts/Drug/DrugController.js
+++ app/assets/javascripts/Drug/DrugController.js
@@ -1,50 +1,38 @@
-app.controller("DrugController", ['$scope', '$routeParams', '$location', '$sanitize', 'DrugService', 'EventService',
-function ($scope, $routeParams, $location, $sanitize, drugService, eventService) {
+app.controller("DrugController", ['$scope', '$routeParams', '$location', 'DrugService', 'EventService',
+function ($scope, $routeParams, $location, drugService, eventService) {
+    $scope.selectedDrug = {}
+    $scope.drug = null;
+    $scope.eventsDetail = null;
 
-  window.DrugControllerScope = $scope; // debugging hook TODO: remove
-  $scope.selectedDrug = {}
-  $scope.drug = null,
-  $scope.eventsDetail = null;
-  $scope.selectedLabel = null;
-  $scope.searchPlaceholder = "enter drug name (e.g. Lipitor)";
+    // fetch details for a given drug
+    $scope.getDetail = function () {
+        drugService.getDetails($scope.selectedDrug.product_ndc, $scope.onDetailsLoaded);
+    }
 
-  // typeahead search
-  $scope.searchDrugs = function (val) {
-      return drugService.typeAheadSearch(val);
-  };
-  $scope.onSelect = function (item, model, label) {
-    $scope.navigateToDrug(item.product_ndc)
-  };
+    $scope.onDetailsLoaded = function (data) {
+        $scope.drug = data;
+        $scope.drillOnEvent(''); // get a sampling of recent events
+    }
 
-  // fetch details for a given drug
-  $scope.getDetail = function () {
-      drugService.getDetails($scope.selectedDrug.product_ndc, $scope.onDetailsLoaded);
-  }
+    // fetch event details for table
+    $scope.drillOnEvent = function (term) {
+        eventService.index($scope.drug.proprietary_name, term, $scope.onEventsLoaded);
+    }
 
-  $scope.onDetailsLoaded = function(data){
-      $scope.drug = data;
-      $scope.drillOnEvent(''); // get a sampling of recent events
-  }
+    $scope.onEventsLoaded = function (data) {
+        $scope.eventsDetail = data.results;
+    }
 
-  // fetch event details for table
-  $scope.drillOnEvent = function (term) {
-      eventService.index($scope.drug.proprietary_name, term, $scope.onEventsLoaded);
-  }
+    // navigate among drugs
+    $scope.navigateToDrug = function (product_ndc) {
+        return $location.path("/drug/" + product_ndc);
+    }
 
-  $scope.onEventsLoaded = function (data) {
-      $scope.eventsDetail = data.results;
-  }
-
-  // navigate among drugs
-  $scope.navigateToDrug = function(product_ndc) {
-    return $location.path("/drug/" + product_ndc);
-  }
-
-  // if we have a drug ID via the route, use that
-  if ($routeParams.product_ndc) {
-    $scope.drugLoading = true;
-    $scope.selectedDrug.product_ndc = $routeParams.product_ndc;
-    $scope.getDetail();
-  }
+    // if we have a drug ID via the route, use that
+    if ($routeParams.product_ndc) {
+        $scope.drugLoading = true;
+        $scope.selectedDrug.product_ndc = $routeParams.product_ndc;
+        $scope.getDetail();
+    }
 
 }]);
---
+++ app/assets/javascripts/Search/SearchBox.js
@@ -0,0 +1,45 @@
+﻿app.controller("SearchController", 
+function ($rootScope, $scope, $location, DrugService) {
+    $scope.currentPath = '/';
+    $scope.hide = false;
+
+    $scope.reset = function () {
+        $scope.selectedLabel = null;
+        $scope.searchPlaceholder = "enter drug name (e.g. Lipitor)";
+    };
+
+    $scope.searchDrugs = function (val) {
+        return DrugService.typeAheadSearch(val);
+    };
+
+    $scope.onSelect = function (item, model, label) {
+        $scope.reset();
+        return $location.path("/drug/" + item.product_ndc);
+    };
+
+    $scope.onNewLocation = function (newValue, oldValue) {
+        $scope.currentPath = newValue;
+        if( $scope.hideOnHome || null )
+            $scope.hide = ($scope.currentPath == '/')
+    };
+
+    $scope.reset();
+});
+
+app.directive('searchBox', function ($location) {
+    var chart = {
+        restrict: 'EA',
+        scope: {
+            hideOnHome: '='
+        },
+        templateUrl: 'Search/searchTemplate.html',
+        controller: 'SearchController',
+        link: function (scope, element, attrs) {
+            scope.$watch(function () {
+                return $location.$$path;
+            }, scope.onNewLocation);
+        }
+    }
+
+    return chart;
+});
