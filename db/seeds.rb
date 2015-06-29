4d3935cc6b855cd77210bae55e7bd3c2f48b4c74
Jeffrey M Farley <JeffreyMFarley@users.noreply.github.com>
2015-06-29 16:09:43 -0400

    seed.rb checks for record count before seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,65 +2,85 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 # Core product list
-puts 'Bulk Load - Product NDC'
-CSV.foreach('data/product_ndc.txt', :col_sep => "\t",:headers => true, ) do |row|
-    row = row.to_hash.delete_if{|k,v|k.blank?}
-    Drug.create(row)
+if Drug.count == 0 
+	puts 'Bulk Load - Product NDC'
+	CSV.foreach('data/product_ndc.txt', :col_sep => "\t",:headers => true, ) do |row|
+		row = row.to_hash.delete_if{|k,v|k.blank?}
+		Drug.create(row)
+	end
 end
 
-puts 'Bulk Load - Substances'
-CSV.foreach('data/openfda-substance_name.txt', :col_sep => "\t",:headers => true, ) do |row|
-    row = row.to_hash.delete_if{|k,v|k.blank?}
-    Substance.create(row)
+if Substance.count == 0
+	puts 'Bulk Load - Substances'
+	CSV.foreach('data/openfda-substance_name.txt', :col_sep => "\t",:headers => true, ) do |row|
+		row = row.to_hash.delete_if{|k,v|k.blank?}
+		Substance.create(row)
+	end
 end
 
-puts 'Bulk Load - Manufacturers'
-CSV.foreach('data/openfda-manufacturer_name.txt', :col_sep => "\t",:headers => true, ) do |row|
-    row = row.to_hash.delete_if{|k,v|k.blank?}
-    Manufacturer.create(row)
+if Manufacturer.count == 0 
+	puts 'Bulk Load - Manufacturers'
+	CSV.foreach('data/openfda-manufacturer_name.txt', :col_sep => "\t",:headers => true, ) do |row|
+		row = row.to_hash.delete_if{|k,v|k.blank?}
+		Manufacturer.create(row)
+	end
 end
 
-puts 'Bulk Load - Pharma Class (Chemical Structure)'
-CSV.foreach('data/openfda-pharm_class_cs.txt', :col_sep => "\t",:headers => true, ) do |row|
-    row = row.to_hash.delete_if{|k,v|k.blank?}
-	PharmaClassChemical.create(row)
+if PharmaClassChemical.count == 0 
+	puts 'Bulk Load - Pharma Class (Chemical Structure)'
+	CSV.foreach('data/openfda-pharm_class_cs.txt', :col_sep => "\t",:headers => true, ) do |row|
+		row = row.to_hash.delete_if{|k,v|k.blank?}
+		PharmaClassChemical.create(row)
+	end
 end
 
-puts 'Bulk Load - Pharma Class (Established Pharma Class)'
-CSV.foreach('data/openfda-pharm_class_epc.txt', :col_sep => "\t",:headers => true, ) do |row|
-    row = row.to_hash.delete_if{|k,v|k.blank?}
-	PharmaClassEstablished.create(row)
+if PharmaClassEstablished.count == 0 
+	puts 'Bulk Load - Pharma Class (Established Pharma Class)'
+	CSV.foreach('data/openfda-pharm_class_epc.txt', :col_sep => "\t",:headers => true, ) do |row|
+		row = row.to_hash.delete_if{|k,v|k.blank?}
+		PharmaClassEstablished.create(row)
+	end
 end
 
-puts 'Bulk Load - Pharma Class (Method of Action)'
-CSV.foreach('data/openfda-pharm_class_moa.txt', :col_sep => "\t",:headers => true, ) do |row|
-    row = row.to_hash.delete_if{|k,v|k.blank?}
-	PharmaClassMethod.create(row)
+if PharmaClassMethod.count == 0
+	puts 'Bulk Load - Pharma Class (Method of Action)'
+	CSV.foreach('data/openfda-pharm_class_moa.txt', :col_sep => "\t",:headers => true, ) do |row|
+		row = row.to_hash.delete_if{|k,v|k.blank?}
+		PharmaClassMethod.create(row)
+	end
 end
 
-puts 'Bulk Load - Pharma Class (Physiologic Effect)'
-CSV.foreach('data/openfda-pharm_class_pe.txt', :col_sep => "\t",:headers => true, ) do |row|
-    row = row.to_hash.delete_if{|k,v|k.blank?}
-	PharmaClassPhysiologic.create(row)
+if PharmaClassPhysiologic.count == 0 
+	puts 'Bulk Load - Pharma Class (Physiologic Effect)'
+	CSV.foreach('data/openfda-pharm_class_pe.txt', :col_sep => "\t",:headers => true, ) do |row|
+		row = row.to_hash.delete_if{|k,v|k.blank?}
+		PharmaClassPhysiologic.create(row)
+	end
 end
 
-puts 'Bulk Load - Product Type'
-CSV.foreach('data/openfda-product_type.txt', :col_sep => "\t",:headers => true, ) do |row|
-    row = row.to_hash.delete_if{|k,v|k.blank?}
-	ProductType.create(row)
+if ProductType.count == 0
+	puts 'Bulk Load - Product Type'
+	CSV.foreach('data/openfda-product_type.txt', :col_sep => "\t",:headers => true, ) do |row|
+		row = row.to_hash.delete_if{|k,v|k.blank?}
+		ProductType.create(row)
+	end
 end
 
-puts 'Bulk Load - Route'
-CSV.foreach('data/openfda-route.txt', :col_sep => "\t",:headers => true, ) do |row|
-    row = row.to_hash.delete_if{|k,v|k.blank?}
-	Route.create(row)
+if Route.count == 0
+	puts 'Bulk Load - Route'
+	CSV.foreach('data/openfda-route.txt', :col_sep => "\t",:headers => true, ) do |row|
+		row = row.to_hash.delete_if{|k,v|k.blank?}
+		Route.create(row)
+	end
 end
 
-# Pricing data
-NadacService.read_workbook
+if ServiceCache.count == 0 
+	# Pricing data
+	NadacService.read_workbook
 
-# Medication Guide links & data 
-FdaMedicationGuideService.import_data
+	# Medication Guide links & data 
+	FdaMedicationGuideService.import_data
 
-# Shortage/Discontinuation data
-FdaShortageService.import_data
+	# Shortage/Discontinuation data
+	FdaShortageService.import_data
+end
