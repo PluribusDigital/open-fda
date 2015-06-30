abb805820a00078c73fa78920b8d8f6b173b5b6b
Chris Cassatt <chris.cassatt@gmail.com>
2015-06-29 23:17:32 -0400

    Tagged docker image as latest

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -6,9 +6,16 @@ DOCKERRUN_FILE=Dockerrun.aws.json
 # Deploy web image to Docker Hub
 docker push stsilabs/openfda-web:$CIRCLE_BUILD_NUM
 
+if [ "$DOCKER_IMAGE_LATEST" = "true"  ]
+then
+  # Tag as latest and push again
+  docker tag stsilabs/openfda-web:$CIRCLE_BUILD_NUM stsilabs/openfda-web:latest
+fi
+
 # Create new Elastic Beanstalk version
 EB_BUCKET=open-fda
 
+cd deploy/beanstalk
 # variable substitutions
 sed -e "s/<TAG>/$CIRCLE_BUILD_NUM/" \
     -e "s/<POSTGRES_USER>/$POSTGRES_USER/" \
@@ -19,7 +26,6 @@ sed -e "s/<TAG>/$CIRCLE_BUILD_NUM/" \
     < $DOCKERRUN_FILE.template > $DOCKERRUN_FILE
 
 # elastic beanstalk requires application source to be zipped
-cd deploy/beanstalk
 zip -r $DOCKERRUN_FILE.zip $DOCKERRUN_FILE .ebextensions
 
 aws s3 cp $DOCKERRUN_FILE.zip s3://$EB_BUCKET/$DOCKERRUN_FILE.zip
