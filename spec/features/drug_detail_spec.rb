df8fe4d21b526edeb16111d2067b93905b257c65
Jeffrey M Farley <JeffreyMFarley@users.noreply.github.com>
2016-04-14 09:01:40 -0400

    Updating smoke tests. Underlying text has changed since 7-2015

--- spec/features/drug_detail_spec.rb
+++ spec/features/drug_detail_spec.rb
@@ -15,7 +15,7 @@ feature "Drug Detail", js: true do
     # Generic Name
     expect(page).to have_content "SILDENAFIL CITRATE"
     # Effects 
-    expect(page).to have_content "PRURITUS"
+    expect(page).to have_content "MYOCARDIAL"
   end # typeahead
 
   scenario "info on recalls" do
